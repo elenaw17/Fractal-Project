835fbf68ca1337347f07dffaa435fa7e38bc0d59
elenaw17 <89656412+elenaw17@users.noreply.github.com>
2021-12-17 20:51:53 -0500

    update index2.js

--- index2.js
+++ index2.js
@@ -14,6 +14,8 @@ that can be used in the drawLinesFromPoints function.
 */
 var seed = [[0, 0], [100, 100], [200, 0], [300, 100]];
 
+var originalFigure = [[0, 200], [400, 200]];
+
 
 /*
 The function takes in a context variable for the canvas that it draws on.
@@ -157,9 +159,52 @@ function translateFigure(lineMatrix, targetPoint) {
     return newMatrix;
 }
 
+/* 
+Finds and returns the angle between two points
+takes in two matrices, each with an x and y value 
+point1 is startPoint, point2 is endPoint
+Sets the end point to what it would be if the start point was at (0, 0)
+tan(angle) = y value of endpoint / x value of endpoint
+arctan(tan(angle)) = angle
+*/
+function findAngle(point1, point2) {
+    var endX = point2[0] - point1[0];
+    var endY = point2[1] - point1[1];
+    var angle;
+    if (endX === 0) {
+        angle = Math.atan(endY / endX);
+    }
+    else if (endY > 0) {
+        angle = Math.PI / 2;
+    }
+    else if (endY < 0) {
+        angle = Math.PI * 3 / 2;
+    }
+    else {
+        // default value if angle cannot be found
+        angle = 0;
+    }
+    return angle; 
+}
+
+/* 
+Finds and returns the distance between two points
+takes in two matrices, each with an x and y value
+point1 is startPoint, point2 is endPoint
+Sets the end point to what it would be if the start point was at (0, 0)
+distance = sqrt((xdistance)²+(ydistance)²), applying the distance formula
+*/
+function findDistance(point1, point2) {
+    var endX = point2[0] - point1[0];
+    var endY = point2[1] - point1[0];
+    var distance = Math.sqrt((endX * endx) + (endY * endY));
+    return distance;
+}
+
 
 var fractalContext = document.getElementById("fractal").getContext("2d");
 
+/*
 drawLinesFromPoints(fractalContext, seed);
 
 drawLinesFromPoints(fractalContext, translateFigure(seed, [40, 100]));
@@ -167,8 +212,82 @@ drawLinesFromPoints(fractalContext, translateFigure(seed, [40, 100]));
 drawLinesFromPoints(fractalContext, rotateFigure(seed, Math.PI * 1 / 6));
 
 drawLinesFromPoints(fractalContext, scaleFigure(seed, 0.75));
+*/
 
+function drawLineFractal(fractalSeed, firstFigure, iterations) {
+    // matrix to hold all iterations
+    var newMatrix = [];
+    // matrix to hold 1 iteration before it is passed to the other matrix
+    var matrix;
+    var startPoint;
+    var endPoint;
+    // matrix to hold the rotated, scaled, and translated seed values
+    var transformSeed;
 
-function drawLineFractal() {
+    // Angle Variables: 
+    // angle that you want to rotate to
+    var targetAngle;
+    // seedAngle + angleChange = targetAngle, angleChange = targetAngle - seedAngle
+    // angle that you want to rotate by
+    var angleChange;
+    // finds angle between the first and last points of the seed
+    var seedAngle = findAngle(fractalSeed[0], fractalSeed[fractalSeed.length - 1]);
     
+    // Distance Variables
+    // distance that you want to scale up/down to
+    var targetDistance;
+    // seedDistance * distanceChange = targetDistance, distanceChange = targetDistance / seedDistance
+    // scaling factor that you want to multiply by
+    var distanceChange;
+    // finds distance between the first and last points of the seed
+    var seedDistance = findDistance(fractalSeed[0], fractalSeed[fractalSeed.length - 1]);
+    // prevents the function from dividing by 0
+    if (seedDistance === 0) {
+        seedDistance = 1;
+    }
+    
+    // Translation Variables:
+    var targetPoint;
+    
+    var i; // iterator for fractal iterations
+    var j; // iterator for number of points per iteration
+    var k; // iterator for transformSeed
+    
+    newMatrix[0] = firstFigure;
+    
+    for (i = 1; i <= iterations; i++) {
+        matrix = [];
+        // iterates through all points of the most recent fractal iteration
+        // replaces lines with the seed and adds the new coordinates to the next layer of the matrix
+        for (j = 1; j < newMatrix[i - 1].length; j++) {
+            // getting start and end points
+            startPoint = newMatrix[i - 1][j - 1];
+            endPoint = newMatrix[i - 1][j];
+            
+            // rotation
+            targetAngle = findAngle(startPoint, endPoint);
+            angleChange = seedAngle - targetAngle;
+            transformSeed = rotateFigure(fractalSeed, angleChange);
+            
+            // scaling
+            targetDistance = findDistance(startPoint, endPoint);
+            distanceChange = targetDistance / seedDistance;
+            transformSeed = scaleFigure(transformSeed, distanceChange);
+            
+            // translation
+            transformSeed = translateFigure(transformSeed, [startPoint, endPoint]);
+            
+            // putting transformSeed in the matrix (without including the last point, preventing overlap)
+            for (i = 0; i < transformSeed.length - 1; i++) {
+                matrix.push(transformSeed[i]);
+            }
+        }
+        // adding the last value to matrix
+        matrix.push(transformSeed[transformSeed.length - 1]);
+        newMatrix.push(matrix);
+    }
+    return newMatrix;
 }
+
+let fractal = drawLineFractal(seed, originalFigure, 1);
+drawLinesFromPoints(fractalContext, fractal[1]);
